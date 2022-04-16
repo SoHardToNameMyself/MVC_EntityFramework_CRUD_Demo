f06109bd555e92ee2d5b4ffa780b17c462135adc
yicheng <yichengfeng.work@gmail.com>
2022-04-16 16:59:35 -0500

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_EntityFramework_CRUD_Demo.Models;
+
+namespace MVC_EntityFramework_CRUD_Demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeDBEntities db = new EmployeeDBEntities();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(db.EmployeeDetails.ToList());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            if (employeeDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeDetail);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmpID,Name,Address,Age,Salary,worktype")] EmployeeDetail employeeDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmployeeDetails.Add(employeeDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employeeDetail);
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            if (employeeDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeDetail);
+        }
+
+        // POST: Employee/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmpID,Name,Address,Age,Salary,worktype")] EmployeeDetail employeeDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employeeDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employeeDetail);
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            if (employeeDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employeeDetail);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EmployeeDetail employeeDetail = db.EmployeeDetails.Find(id);
+            db.EmployeeDetails.Remove(employeeDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_EntityFramework_CRUD_Demo.MvcApplication" Language="C#" %>
